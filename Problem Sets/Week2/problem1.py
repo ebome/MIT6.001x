5fb09365577fc03bb1fc9001295c52b22b0c35da
Alisa Gao <36057877+ebome@users.noreply.github.com>
2018-07-12 16:10:21 +1000

    Week2 problem1
    The basic idea extended to problem 2 and 3

--- Problem Sets/Week2/problem1.py
+++ Problem Sets/Week2/problem1.py
@@ -1,8 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jun  25 13:41:10 2018
+Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit card company each month.
 
-@author: Ebome
+The following variables contain values as described below:
+
+balance - the outstanding balance on the credit card
+
+annualInterestRate - annual interest rate as a decimal
+
+monthlyPaymentRate - minimum monthly payment rate as a decimal
+
+For each month, calculate statements on the monthly payment and 
+remaining balance. At the end of 12 months, print out the remaining
+balance. Be sure to print out no more than two decimal digits of 
+accuracy - so print
+
+Remaining balance: 813.41
+
+instead of
+
+Remaining balance: 813.4141998135 
+
+So your program only prints out one thing: the remaining balance at 
+the end of the year in the format:
+
+Remaining balance: 4784.0
+A summary of the required math is found below:
+
+Monthly interest rate= (Annual interest rate) / 12.0
+Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
+Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
+Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
 """
 balance=4773 # balance = outstanding balance (未偿付余额)
 annualInterestRate=0.2
