4fb7c6ab0e796b8fbee1d19f5af3d3fe54847bc5
Alisa Gao <36057877+ebome@users.noreply.github.com>
2018-07-12 16:06:56 +1000

    Week1 problem2

--- Problem Sets/Week1/problem2.py
+++ Problem Sets/Week1/problem2.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
 """
-Created on Wed Apr 18 21:27:16 2018
-
-@author: Ebome
-
 Assume s is a string of lower case characters.
 
 Write a program that prints the number of times the string 'bob' occurs in s.
