4eea39fad6011ac978d1c3d1dd427a4b29b7a140
Alisa Gao <36057877+ebome@users.noreply.github.com>
2018-07-16 16:17:29 +1000

    Week3 Problem2

--- Problem Sets/Week3/problem2.py
+++ Problem Sets/Week3/problem2.py
@@ -1,5 +1,7 @@
 '''
-Implement the function getGuessedWord that takes in two parameters - a string, secretWord, and a list of letters, lettersGuessed. This function returns a string that is comprised of letters and underscores, based on what letters in lettersGuessed are in secretWord. This shouldn't be too different from isWordGuessed!
+Implement the function getGuessedWord that takes in two parameters - a string, secretWord, and a list of letters, lettersGuessed. 
+This function returns a string that is comprised of letters and underscores, based on what letters in lettersGuessed are in secretWord. 
+This shouldn't be too different from isWordGuessed!
 
 Example Usage:
 
@@ -7,6 +9,9 @@ Example Usage:
 >>> lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
 >>> print(getGuessedWord(secretWord, lettersGuessed))
 '_ pp_ e'
+
+When inserting underscores into your string, it's a good idea to add at least a space after each one, so it's clear to the user 
+how many unguessed letters are left in the string (compare the readability of ____ with _ _ _ _ ). This is called usability
 
 For this function, you may assume that all the letters in secretWord and lettersGuessed are lowercase.
 '''
