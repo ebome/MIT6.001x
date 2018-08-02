93429768a95b9a7edcaf019373941b16cca10f0b
Yang Gao <36057877+ebome@users.noreply.github.com>
2018-08-02 22:02:15 +1000

    Add files via upload

---
+++ Mid Term/q9.py
@@ -0,0 +1,10 @@
+# Write a function to flatten a list. The list contains other lists, strings, or ints. For example, [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+# is flattened into [1,'a','cat',2,3,'dog',4,5] (order matters).
+
+def flatten(aList):
+    ''' 
+    aList: a list 
+    Returns a copy of aList, which is a flattened version of aList 
+    '''
+    a = eval(  '[' +  str(aList).replace('[', '').replace(']', '')  +  ']'   )
+    return a
