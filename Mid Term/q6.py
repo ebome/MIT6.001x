88238040c6f697f32da86e703644b38ab85aa877
Yang Gao <36057877+ebome@users.noreply.github.com>
2018-08-03 16:36:49 +1000

    Add problem 6 from Edx

---
+++ Mid Term/q6.py
@@ -0,0 +1,18 @@
+'''
+Write a recursive Python function, given a non-negative integer N, to calculate and return the sum of its digits.
+
+Hint: Mod (%) by 10 gives you the rightmost digit (126 % 10 is 6), while doing integer division by 10 removes the rightmost digit (126 // 10 is 12).
+
+This function has to be recursive; you may not use loops!
+
+This function takes in one integer and returns one integer.
+'''
+def sumDigits(N):
+    '''
+    N: a non-negative integer
+    '''
+    if N < 10:
+        return N
+    else:
+        return sumDigits(N//10) + N % 10
+# correct
