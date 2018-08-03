a8b47810dafa514112e0142a9af3b149902bd0e3
Yang Gao <36057877+ebome@users.noreply.github.com>
2018-08-03 16:33:57 +1000

    Add problem 4 from Edx

--- Mid Term/q5.py
+++ Mid Term/q5.py
@@ -1,20 +1,20 @@
 '''
-Write a Python function that returns the sum of the pairwise products of listA and listB. You should assume that listA and listB have
-the same length and are two lists of integer numbers. For example, if listA = [1, 2, 3] and listB = [4, 5, 6], the dot product is
-1*4 + 2*5 + 3*6, meaning your function should return: 32
+Write a function called getSublists, which takes as parameters a list of integers named L and an integer named n.
 
- Hint: You will need to traverse both lists in parallel.
+assume L is not empty
+assume 0 < n <= len(L)
+This function returns a list of all possible sublists in L of length n without skipping elements in L. The sublists in the returned list should be ordered in the way they appear in L, with those sublists starting from a smaller index being at the front of the list.
 
- This function takes in two lists of numbers and returns a number.
+Example 1, if L = [10, 4, 6, 8, 3, 4, 5, 7, 7, 2] and n = 4 then your function should return the list [[10, 4, 6, 8], [4, 6, 8, 3], [6, 8, 3, 4], [8, 3, 4, 5], [3, 4, 5, 7], [4, 5, 7, 7], [5, 7, 7, 2]]
 
-def dotProduct(listA, listB):
+Example 2, if L = [1, 1, 1, 1, 4] and n = 2 then your function should return the list [[1, 1], [1, 1], [1, 1], [1, 4]]
 
-#    listA: a list of numbers
-#    listB: a list of numbers of the same length as listA
+
+Your function does not have to be recursive. Do not leave any debugging print statements when you paste your code in the box.
 '''
-def dotProduct(listA, listB):
-    summation=0
-    for i in range(len(listA)):
-        summation += listA[i]*listB[i]        
-    
-    return summation
+def getSublists(L, n):
+    subList=[]
+    for i in range(len(L)-n+1):
+       subList.append( L[i:(i+n)] )
+    return subList
+# correct   
