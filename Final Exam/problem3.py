b722a0640fc33e6ad2334a4e2ba1efff9edd952e
Yang Gao <36057877+ebome@users.noreply.github.com>
2018-09-02 13:44:29 +1000

    Q3 check palindrome without reverse()

--- Final Exam/problem3.py
+++ Final Exam/problem3.py
@@ -1,1 +1,19 @@
+'''
+Determine if a string is a palindrome (reads the same forwards and backwards)
+DO NOT use reverse() or aString[:,:,-1] in Python
+'''
+def isPalindrome(s):
+    '''
+    s is a String
+    '''
+    length=len(s)
+    i = 0
+    while i < length//2 :
+        i += 1
+        if s[i] !=s[length-i-1]:
+            return False;
+    return True;
 
+# Test case:
+# s = 'abcba'
+# palindrome = isPalindrome(s)  
